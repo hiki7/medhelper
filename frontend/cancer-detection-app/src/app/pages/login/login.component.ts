8eecc2fa8de945fe7f4dd93bd887fee1f3e12c44
hiki7 <rsaliyev03@gmail.com>
2024-10-18 19:56:29 +0500

    add: service for auth and changed the logic of login

--- frontend/cancer-detection-app/src/app/pages/login/login.component.ts
+++ frontend/cancer-detection-app/src/app/pages/login/login.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../auth.service';  // Adjust the path as necessary
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  title: string = 'Connection to Django app';
+  logged: boolean = false;
+  email: string = '';
+  password: string = '';
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    const access: string | null = localStorage.getItem('access');
+    if (access) {
+      this.logged = true;
+      this.router.navigate(['/chat']);  // Redirect if already logged in
+    }
+  }
+
+  login(): void {
+    this.authService.login(this.email, this.password).subscribe((data) => {
+      if (data.access) {
+        this.logged = true;
+        localStorage.setItem('access', data.access);
+        localStorage.setItem('refresh', data.refresh);
+        this.router.navigate(['/chat']);  // Redirect to profile on success
+      }
+    }, error => {
+      alert('Login failed. Please check your credentials.');
+    });
+  }
+
+  logout(): void {
+    this.logged = false;
+    localStorage.removeItem('access');
+    localStorage.removeItem('refresh');
+  }
 }
