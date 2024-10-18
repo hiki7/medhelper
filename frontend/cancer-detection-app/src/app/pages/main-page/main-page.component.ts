3fde57742a02d3efdc45ed208b6fe54a9697ee20
hiki7 <rsaliyev03@gmail.com>
2024-10-18 18:50:26 +0500

    add: configured main-page sample

--- frontend/cancer-detection-app/src/app/pages/main-page/main-page.component.ts
+++ frontend/cancer-detection-app/src/app/pages/main-page/main-page.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrl: './main-page.component.css'
+  styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent {
 
+  constructor(private router: Router) {}
+
+  goToChat() {
+    this.router.navigate(['/chat']);
+  }
 }
